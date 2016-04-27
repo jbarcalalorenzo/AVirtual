123671dc00b57fdc1756682d715950569a8e5dd7
javierbl <javier.barcala@balidea.com>
2016-04-27 16:47:58 +0200

    Add style for django-tables2

--- static/js/main.js
+++ static/js/main.js
@@ -2,30 +2,79 @@
  * Created by javierbl on 26/04/2016.
  */
 $(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
 
     trigger.click(function () {
-      hamburger_cross();
+        hamburger_cross();
     });
 
     function hamburger_cross() {
 
-      if (isClosed == true) {
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
 
-  $('[data-toggle="offcanvas"]').click(function () {
+    $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
-  });
+    });
+    var table = $('#table');
+
+    // Table bordered
+    $('#table-bordered').change(function () {
+        var value = $(this).val();
+        table.removeClass('table-bordered').addClass(value);
+    });
+
+    // Table striped
+    $('#table-striped').change(function () {
+        var value = $(this).val();
+        table.removeClass('table-striped').addClass(value);
+    });
+
+    // Table hover
+    $('#table-hover').change(function () {
+        var value = $(this).val();
+        table.removeClass('table-hover').addClass(value);
+    });
+
+    // Table color
+    $('#table-color').change(function () {
+        var value = $(this).val();
+        table.removeClass(/^table-mc-/).addClass(value);
+    });
 });
+(function (removeClass) {
+
+    jQuery.fn.removeClass = function (value) {
+        if (value && typeof value.test === "function") {
+            for (var i = 0, l = this.length; i < l; i++) {
+                var elem = this[i];
+                if (elem.nodeType === 1 && elem.className) {
+                    var classNames = elem.className.split(/\s+/);
+
+                    for (var n = classNames.length; n--;) {
+                        if (value.test(classNames[n])) {
+                            classNames.splice(n, 1);
+                        }
+                    }
+                    elem.className = jQuery.trim(classNames.join(" "));
+                }
+            }
+        } else {
+            removeClass.call(this, value);
+        }
+        return this;
+    }
+
+})(jQuery.fn.removeClass);
